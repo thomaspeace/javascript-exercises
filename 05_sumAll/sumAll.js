5bee4bc0d196888319d465f97b9188a3392bf6a0
Thomas Peace <thomasjpeace29@gmail.com>
2024-11-12 22:31:13 +0000

    finish exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(lowNum, highNum) {
+    if (lowNum < 0 || highNum < 0) {return 'ERROR'};
+    if (typeof lowNum != 'number' || typeof highNum != 'number') {return 'ERROR'};
+    if (lowNum % 1 != 0 || highNum % 1 != 0) {return 'ERROR'};
 
+    let total = 0;
+
+    if (lowNum < highNum) {
+        for(let i = lowNum; i <= highNum; i++) {
+            total += i;
+        };
+    } else {
+        for(let i = highNum; i <= lowNum; i++) {
+            total += i;
+        };
+    };
+    return total;
 };
 
 // Do not edit below this line
