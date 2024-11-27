9e8814db34df1892fe5652d4c46b8a866cd3955d
Thomas Peace <thomasjpeace29@gmail.com>
2024-11-27 12:15:31 +0000

    add palinindrome answer

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedString = str
+        .toLowerCase()
+        .split('')
+        .filter(char => alphanumerical.includes(char))
+        .join('');
+
+    const reversedCleanString = cleanedString.split('').reverse().join('');
+
+    return cleanedString === reversedCleanString;
+    
 };
 
 // Do not edit below this line
