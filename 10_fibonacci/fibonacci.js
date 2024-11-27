c70eca0338fb36b9997a28c83f35fac60903ea14
Thomas Peace <thomasjpeace29@gmail.com>
2024-11-27 18:40:10 +0000

    add js exercsies 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return 'OOPS';
+    }
+    if (number == 0) {
+        return 0
+    }
+    if (number == 1 || number == 2) {
+        return 1
+    }
+    
+    number = parseInt(number);
 
+    let fibArray = [1, 1];
+
+    for (let i = 0; i < number - 2; i++) {
+        fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+    }
+    return fibArray.pop()
 };
 
 // Do not edit below this line
