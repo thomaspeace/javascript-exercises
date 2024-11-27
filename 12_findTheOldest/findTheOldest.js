54eb7692bcbe79f8074e30cf56606f015c27774a
Thomas Peace <thomasjpeace29@gmail.com>
2024-11-27 18:59:24 +0000

    add js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((oldestPerson, currentPerson) => {
+        const oldestPersonAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson;
+    }, people[0]);
+    return oldest;
+};
 
+const getAge = (birth, death) => {
+    if(!death) {
+        const currentYear = new Date().getFullYear();
+        return currentYear - birth;
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
